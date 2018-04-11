0934a4e09ff68473d88854aad0aa06467a509988
jamalw <jamalw@princeton.edu>
2018-04-11 19:35:24 -0400

    now this script plots the human bound matching searchlight results so i need to change the script name soon

--- plot_avg_event_lengths_split.py
+++ plot_avg_event_lengths_split.py
@@ -4,7 +4,7 @@ import scipy
 import matplotlib.pyplot as plt
 import nibabel as nib
 import numpy as np
-
+import sys
 """
 Bring up the visualization window.
 """
@@ -12,23 +12,30 @@ Bring up the visualization window.
 view = "lateral"
 hemi = "split"
 
-length = str(10)
+#length = str(sys.argv[1])
 
-brain = Brain("fsaverage", hemi, "inflated", title=length + " Second Events",cortex='low_contrast',views=['lat','med'],background="white")
+brain = Brain("fsaverage", hemi, "inflated", title="zstats human bounds (corrected) srm k=30",cortex='low_contrast',views=['lat','med'],background="white")
 
 """
 Get a path to the overlay file.
 """
-mri_file = '/Users/jamalw/Desktop/PNI/music_event_structures/' + length + '_sec_events.nii.gz'
+mri_file = '/Users/jamalw/Desktop/PNI/music_event_structures/zstats_map.nii.gz'
 reg_file = '/Applications/freesurfer/average/mni152.register.dat'
 
 data = nib.load(mri_file)
-minval = 0
-maxval = np.max(data.get_data())
-#maxval = .0389 
+minval = 0.2
+maxval = np.max(data.get_data()) 
 
 surf_data_lh = project_volume_data(mri_file,"lh",reg_file)
 surf_data_rh = project_volume_data(mri_file,"rh",reg_file)
+
+qmri_file = '/Users/jamalw/Desktop/PNI/music_event_structures/qstats_map.nii.gz'
+
+qdata_lh = project_volume_data(qmri_file,"lh",reg_file)
+surf_data_lh[np.logical_or(qdata_lh>0.005,surf_data_lh<0)] = 0
+
+qdata_rh = project_volume_data(qmri_file,"rh",reg_file)
+surf_data_rh[np.logical_or(qdata_rh>0.005, surf_data_rh<0)] = 0
 
 brain.add_overlay(surf_data_lh,min=minval,max=maxval,name="thirty_sec_lh", hemi='lh',sign="pos")
 brain.add_overlay(surf_data_rh,min=minval,max=maxval,name="thirty_sec_rh", hemi='rh',sign="pos")
@@ -43,7 +50,7 @@ brain.add_overlay(surf_data_rh,min=minval,max=maxval,name="thirty_sec_rh", hemi=
 #brain.add_data(surf_data_rh, minval,maxval, colormap="CMRmap", alpha=1,
 #               hemi='rh')
 #
-brain.save_image(length + "_sec_events.png")
+brain.save_image("zstats_human_bounds_srm_k30.png")
 #
 mlab.show()
 
