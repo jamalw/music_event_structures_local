f6f1e04c8913999f9d6432008d54670c16457d78
jamalw <jamalw@princeton.edu>
2018-05-13 16:42:21 -0400

    changed script name of plot_avg_event_lengths_split.py to plot_human_bounds_split_view.py

--- plot_human_bounds_split_view.py
+++ plot_human_bounds_split_view.py
@@ -4,35 +4,44 @@ import scipy
 import matplotlib.pyplot as plt
 import nibabel as nib
 import numpy as np
-
+import sys
 """
 Bring up the visualization window.
 """
 #mlab.options.backend = 'envisage'
 view = "lateral"
 hemi = "split"
+srmK = '30'
 
-song_name = 'Waltz_of_Flowers'
+#length = str(sys.argv[1])
 
-brain = Brain("fsaverage", hemi, "inflated", title=song_name,cortex='low_contrast',views=['lat','med'],background="white")
+brain = Brain("fsaverage", hemi, "inflated", title="zstats human bounds (corrected) srm k=" + srmK,cortex='low_contrast',views=['lat','med'],background="white")
 
 """
 Get a path to the overlay file.
 """
-mri_file = '/Users/jamalw/Desktop/PNI/music_event_structures/' + song_name + '/globals_avg_z_n25.nii.gz'
+mri_file = '/Volumes/scratch/jamalw/HMM_searchlight_K_sweep_srm_bound_match/zstats_map_srmK=' + srmK + '.nii.gz'
 reg_file = '/Applications/freesurfer/average/mni152.register.dat'
 
 data = nib.load(mri_file)
-minval = 0.5
-maxval = np.max(data.get_data())
-#maxval = .027
+minval = 0.2
+maxval = np.max(data.get_data()) 
+
 surf_data_lh = project_volume_data(mri_file,"lh",reg_file)
 surf_data_rh = project_volume_data(mri_file,"rh",reg_file)
+
+qmri_file = '/Volumes/scratch/jamalw/HMM_searchlight_K_sweep_srm_bound_match/qstats_map_srmK=' + srmK + '.nii.gz'
+
+qdata_lh = project_volume_data(qmri_file,"lh",reg_file)
+surf_data_lh[np.logical_or(qdata_lh>0.005,surf_data_lh<0)] = 0
+
+qdata_rh = project_volume_data(qmri_file,"rh",reg_file)
+surf_data_rh[np.logical_or(qdata_rh>0.005, surf_data_rh<0)] = 0
 
 brain.add_overlay(surf_data_lh,min=minval,max=maxval,name="thirty_sec_lh", hemi='lh',sign="pos")
 brain.add_overlay(surf_data_rh,min=minval,max=maxval,name="thirty_sec_rh", hemi='rh',sign="pos")
 
-brain.save_image('/Users/jamalw/Desktop/PNI/music_event_structures/' + song_name + '/avg_z_' + song_name + ".png")
+brain.save_image("zstats_human_bounds_srm_k" + srmK + ".png")
 
 mlab.show()
 
