c238696792cc5cc225eef013662bd146822e614e
jamalw <jamalw@princeton.edu>
2018-11-01 13:06:41 -0400

    sfn results

--- plot_ttest_human_bounds_fit_to_all_subjs.py
+++ plot_ttest_human_bounds_fit_to_all_subjs.py
@@ -17,7 +17,7 @@ brain = Brain("fsaverage", hemi, "inflated", title="zstats human bound match fit
 """
 Get a path to the overlay file.
 """
-mri_file = '/Volumes/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/HMM_searchlight_human_bounds_fit_to_all/ttest_results/zstats_map_both_runs.nii.gz'
+mri_file = '/Volumes/jamalw/MES/prototype/link/scripts/data/searchlight_output/HMM_searchlight_human_bounds_fit_to_all/ttest_results/zstats_map_both_runs.nii.gz'
 reg_file = '/Applications/freesurfer/average/mni152.register.dat'
 
 data = nib.load(mri_file)
@@ -27,18 +27,18 @@ maxval = np.max(data.get_data())
 surf_data_lh = project_volume_data(mri_file,"lh",reg_file)
 surf_data_rh = project_volume_data(mri_file,"rh",reg_file)
 
-qmri_file = '/Volumes/norman/jamalw/MES/prototype/link/scripts/data/searchlight_output/HMM_searchlight_human_bounds_fit_to_all/ttest_results/qstats_map_both_runs.nii.gz'
+qmri_file = '/Volumes/jamalw/MES/prototype/link/scripts/data/searchlight_output/HMM_searchlight_human_bounds_fit_to_all/ttest_results/qstats_map_both_runs.nii.gz'
 
 qdata_lh = project_volume_data(qmri_file,"lh",reg_file)
-surf_data_lh[np.logical_or(qdata_lh>0.005,surf_data_lh<0)] = 0
+surf_data_lh[np.logical_or(qdata_lh>0.007,surf_data_lh<0)] = 0
 
 qdata_rh = project_volume_data(qmri_file,"rh",reg_file)
-surf_data_rh[np.logical_or(qdata_rh>0.005, surf_data_rh<0)] = 0
+surf_data_rh[np.logical_or(qdata_rh>0.007, surf_data_rh<0)] = 0
 
 brain.add_overlay(surf_data_lh,min=minval,max=maxval,name="thirty_sec_lh", hemi='lh',sign="pos")
 brain.add_overlay(surf_data_rh,min=minval,max=maxval,name="thirty_sec_rh", hemi='rh',sign="pos")
 
-brain.save_image("human_bounds_match_plots/zstats_ttest_both_runs.png")
+brain.save_image("human_bounds_match_plots/srmk_30/zstats_ttest_both_runs.png")
 
 mlab.show()
 
